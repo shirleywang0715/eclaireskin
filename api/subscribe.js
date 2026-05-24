b58f9f7b5c378ea04b1af292f677e28a8bb5df34
Shirley Wang <shirleywang0715@hotmail.com>
2026-05-25 10:30:03 +1200

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,5 +1,8 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
+  if (req.method !== 'POST') {
+  res.setHeader('Allow', 'POST');
+  return res.status(405).json({ error: 'Method not allowed' });
+}
   
   const { email } = req.body;
   console.log('Email received:', email);
